78db9c02926d2126ad03958f93a606f92b455631
Ariel1776 <arielbulgari1369@gmail.com>
2020-05-25 23:26:43 -0300

    Login quase finalizado

--- TCC/app/src/main/java/com/example/tcc/MainActivity.java
+++ TCC/app/src/main/java/com/example/tcc/MainActivity.java
@@ -2,12 +2,42 @@ package com.example.tcc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText entradaEmail, entradaSenha;
+    private Button buttomLogin, buttomCad;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inicializarComponentes();
+
+        buttomLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = entradaEmail.getText().toString();
+                String senha = entradaSenha.getText().toString();
+
+                Toast.makeText(getApplicationContext(),"Ola" + email +  "Você digitou a senha" + senha,Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+
+    }
+
+    private void inicializarComponentes() {
+        entradaEmail = findViewById(R.id.editEmail);
+        entradaSenha = findViewById(R.id.editSenha);
+        buttomLogin = findViewById(R.id.butaoLog);
+        buttomCad = findViewById(R.id.butaoCad);
     }
 }
