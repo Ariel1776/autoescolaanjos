1ade36d1568311ab6734c56fb4a230e897bac67a
henriquegallodias <henrique0521@hotmail.com>
2020-06-19 06:41:42 -0300

    Versão 0.2 - Adicionada tela principal com Drawer e fragmentos, também icones para opcoes menu e icone do aplicativo.

---
+++ TCC/app/src/main/java/com/example/tcc/FragmentFour.java
@@ -0,0 +1,18 @@
+package com.example.tcc;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentFour extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_four, container, false);
+        return view;
+    }
+}
---
+++ TCC/app/src/main/java/com/example/tcc/FragmentSecond.java
@@ -0,0 +1,20 @@
+package com.example.tcc;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.zip.Inflater;
+
+public class FragmentSecond extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        return view;
+    }
+}
