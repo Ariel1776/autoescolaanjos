dc510c32d8a99d61c3a2eebbedc7e22d8841de6e
henriquegallodias <henrique0521@hotmail.com>
2020-06-25 23:20:39 -0300

    migrate androidX, add firebase cloud firestore e autenth.

--- TCC/app/src/main/java/com/example/tcc/FragmentSecond.java
+++ TCC/app/src/main/java/com/example/tcc/FragmentSecond.java
@@ -1,14 +1,12 @@
 package com.example.tcc;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.util.zip.Inflater;
 
 public class FragmentSecond extends Fragment {
     @Nullable
